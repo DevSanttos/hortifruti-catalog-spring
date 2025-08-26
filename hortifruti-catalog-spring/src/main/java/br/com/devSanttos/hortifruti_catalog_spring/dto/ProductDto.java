0439b6a0923debf4d3bf0502034060ffe1ee898f
Jonathan Rezende dos Santos <161469965+DevSanttos@users.noreply.github.com>
2025-08-26 15:12:43 -0300

    Setando os campos do ProductDto

--- hortifruti-catalog-spring/src/main/java/br/com/devSanttos/hortifruti_catalog_spring/dto/ProductDto.java
+++ hortifruti-catalog-spring/src/main/java/br/com/devSanttos/hortifruti_catalog_spring/dto/ProductDto.java
@@ -1,4 +1,16 @@
 package br.com.devSanttos.hortifruti_catalog_spring.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class ProductDto {
+    private String name;
+    private String description;
+    private Double price;
+    private BigDecimal promoPrice;
+    private LocalDate promoStart;
+    private LocalDate promoEnd;
+    private Boolean active;
+    private String imageUrl;
 }
